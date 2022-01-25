b440425e8b028e3e25f42249a5f86312ddc69dfe
MarketHubb <chris@markethubb.com>
2022-01-25 08:35:27 -0600

    Update: Remove hard-coded link to Google ReCaptcha script

--- wp-content/themes/locks/header.php
+++ wp-content/themes/locks/header.php
@@ -109,7 +109,7 @@
         })(jQuery);
     </script>
 
-    <script src="https://www.google.com/recaptcha/api.js"></script>
+<!--    <script src="https://www.google.com/recaptcha/api.js"></script>-->
 
 </head>
 <body <?php body_class(); ?>>
