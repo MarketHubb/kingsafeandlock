aedc50388e26fac1f356e76afeb87fffba5a3585
MarketHubb <chris@markethubb.com>
2022-03-23 17:48:50 -0500

    Fix: Update modal close trigger for bootstrap 5

--- wp-content/themes/locks/template-parts/modals/content-locksmith.php
+++ wp-content/themes/locks/template-parts/modals/content-locksmith.php
@@ -11,7 +11,7 @@
                         </div>
                     </div>
                 </div>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
