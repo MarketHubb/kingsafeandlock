3f358057bb69a8ba3da528dd7e5076f091a1f1af
MarketHubb <chris@markethubb.com>
2022-08-03 09:44:19 -0400

    Update: Genesis full-width template style updates

---
+++ wp-content/themes/locks/page-templates/full-width.php
@@ -0,0 +1,12 @@
+<?php /* Template Name: Full-width */
+get_header(); ?>
+
+<div class="genesis-container">
+    <?php
+    while ( have_posts() ) : the_post();
+        get_template_part( 'template-parts/content', 'page' );
+    endwhile; // End of the loop.
+    ?>
+</div>
+
+<?php get_footer(); ?>
