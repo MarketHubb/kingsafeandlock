73d207318c620357cb9f2d36de244bde2b52771f
MarketHubb <chris@markethubb.com>
2023-06-29 12:41:10 -0500

    Fix: Replace popup image URL with banner that reads "King Safe & Lock"

--- wp-content/themes/locks/template-parts/modal/content-popup.php
+++ wp-content/themes/locks/template-parts/modal/content-popup.php
@@ -9,7 +9,7 @@
 
 
             <div class="modal-body p-0">
-                <img src="<?php echo home_url() . '/wp-content/uploads/2023/06/Banner-Popup-1.png' ?>" alt="">
+                <img src="<?php echo home_url() . '/wp-content/uploads/2023/06/popup-banner.png' ?>" alt="">
                 <a href="<?php echo get_permalink(4582); ?>" type="button" class="btn btn-primary bg-orange fw-600 rounded-0 border-0 text-white shadow-sm font-lg font-source w-100">Click Here to Learn More</a>
             </div>
 
