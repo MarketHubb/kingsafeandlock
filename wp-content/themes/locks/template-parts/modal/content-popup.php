bf98f2cfc445df5769a8799c3ea6c0b3ae927294
MarketHubb <chris@markethubb.com>
2023-08-30 13:37:39 -0500

    Update: Labor Day Sale

--- wp-content/themes/locks/template-parts/modal/content-popup.php
+++ wp-content/themes/locks/template-parts/modal/content-popup.php
@@ -9,8 +9,8 @@
 
 
             <div class="modal-body p-0">
-                <img src="<?php echo home_url() . '/wp-content/uploads/2023/06/popup-banner.png' ?>" alt="">
-                <a href="<?php echo get_permalink(4582); ?>" type="button" class="btn btn-primary bg-orange fw-600 rounded-0 border-0 text-white shadow-sm font-lg font-source w-100">Click Here to Learn More</a>
+                <img src="<?php echo home_url() . '/wp-content/uploads/2023/08/Labor-Day-Safes.png' ?>" alt="">
+                <a href="<?php echo get_permalink(4624); ?>" type="button" class="btn btn-primary bg-orange fw-600 rounded-0 border-0 text-white shadow-sm font-lg font-source w-100">Click Here to Learn More</a>
             </div>
 
         </div>
