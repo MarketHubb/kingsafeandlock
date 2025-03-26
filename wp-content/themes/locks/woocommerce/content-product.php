5a26e11de67daefc8eb55dedd73e3fbc9d5b51a8
MarketHubb <chris@markethubb.com>
2025-03-26 10:28:50 -0500

    Fix: Update archive-product and content-product WooCommerce template files to latest plugin version

--- wp-content/themes/locks/woocommerce/content-product.php
+++ wp-content/themes/locks/woocommerce/content-product.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying product content within loops
  *
@@ -10,26 +11,31 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see     https://docs.woocommerce.com/document/template-structure/
+ * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.6.0
+ * @version 9.4.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 global $product;
 
-// Ensure visibility.
-if ( empty( $product ) || ! $product->is_visible() ) {
+// Check if the product is a valid WooCommerce product and ensure its visibility before proceeding.
+if (! is_a($product, WC_Product::class) || ! $product->is_visible()) {
     return;
 }
 ?>
 
+<!-- Custom grid output -->
 <div class="col-md-4 text-center mb-3">
     <div class="product-container h-100 px-4 py-5">
-        <h4 class="product-list-name text-black font-weight-bold"><?php echo get_the_title($post->ID); ?></h4>
+        <h4 class="product-list-name text-black font-weight-bold">
+            <?php echo get_the_title($post->ID); ?>
+        </h4>
         <?php if (get_field('post_product_gun_burglary_rating')) { ?>
-            <p><?php echo get_field('post_product_gun_burglary_rating'); ?></p>
+            <p>
+                <?php echo get_field('post_product_gun_burglary_rating'); ?>
+            </p>
         <?php } ?>
         <img src="<?php echo get_the_post_thumbnail_url($post->ID); ?>" />
         <div class="row mt-5 justify-content-center align-items-center inquiry-container mt-3">
@@ -37,52 +43,10 @@ if ( empty( $product ) || ! $product->is_visible() ) {
                 <?php echo get_product_inquiry_btn($post->ID, 'Get Sale Pricing'); ?>
             </div>
             <div class="col-md-12 col-lg-6 mt-4 mt-lg-0">
-                <a class="fw-semibold" href="<?php echo get_permalink($post->ID); ?>">View Safe Details <i class="fas fa-long-arrow-right ms-1"></i></a>
+                <a class="fw-semibold" href="<?php echo get_permalink($post->ID); ?>">View Safe Details <i
+                        class="fas fa-long-arrow-right ms-1"></i></a>
             </div>
 
         </div>
         <a href="<?php echo get_permalink($post->ID); ?>" class="stretched-link"></a>
-    </div>
-</div>
-
-<!--<li --><?php //wc_product_class( '', $product ); ?>
-    <?php
-    /**
-     * Hook: woocommerce_before_shop_loop_item.
-     *
-     * @hooked woocommerce_template_loop_product_link_open - 10
-     */
-    //do_action( 'woocommerce_before_shop_loop_item' );
-
-    /**
-     * Hook: woocommerce_before_shop_loop_item_title.
-     *
-     * @hooked woocommerce_show_product_loop_sale_flash - 10
-     * @hooked woocommerce_template_loop_product_thumbnail - 10
-     */
-    //do_action( 'woocommerce_before_shop_loop_item_title' );
-
-    /**
-     * Hook: woocommerce_shop_loop_item_title.
-     *
-     * @hooked woocommerce_template_loop_product_title - 10
-     */
-    //do_action( 'woocommerce_shop_loop_item_title' );
-
-    /**
-     * Hook: woocommerce_after_shop_loop_item_title.
-     *
-     * @hooked woocommerce_template_loop_rating - 5
-     * @hooked woocommerce_template_loop_price - 10
-     */
-    //do_action( 'woocommerce_after_shop_loop_item_title' );
-
-    /**
-     * Hook: woocommerce_after_shop_loop_item.
-     *
-     * @hooked woocommerce_template_loop_product_link_close - 5
-     * @hooked woocommerce_template_loop_add_to_cart - 10
-     */
-    //do_action( 'woocommerce_after_shop_loop_item' );
-    ?>
-<!--</li>-->
+    </div
